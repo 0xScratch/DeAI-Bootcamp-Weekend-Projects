70f6d1f48c1ba31ca9028bb45e5aebf355e9360a
0xScratch <aryanhunny98@gmail.com>
2025-03-22 19:02:58 +0000

    Created the initial template for week-3 project

---
+++ week-3/app/page.tsx
@@ -0,0 +1,10 @@
+import CharacterManagement from "@/components/ui/character-management";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <CharacterManagement />
+    </div>
+  );
+}
